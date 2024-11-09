6c76e5ecb3c2e5d5e17a129edf71d833c3653651
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-11-09 10:17:58 +0700

    chore(query): configure React Query provider with devtools

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,21 @@
-export default async function HomePage() {
-  return <main>Home</main>;
+'use client';
+
+import { getLocation } from '@/lib/actions';
+import { useQuery } from '@tanstack/react-query';
+
+export default function HomePage() {
+  const { data: locationList } = useQuery({
+    queryKey: ['location'],
+    queryFn: () => getLocation('London'),
+  });
+
+  return (
+    <main>
+      <div>
+        {locationList?.map(({ name, lat }: { name: string; lat: number }) => (
+          <div key={lat + name}>{name}</div>
+        ))}
+      </div>
+    </main>
+  );
 }
