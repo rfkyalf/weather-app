2b8e474bd367a3e3dd64df41fab34af989da99ef
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-11-09 09:54:18 +0700

    chore(api): configure axios instance with base URL and API key for location requests

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,15 +1,18 @@
+import axios from 'axios';
+
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-const API_URL = `http://api.openweathermap.org`;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+const axiosInstance = axios.create({ baseURL: API_URL });
 
 export const getLocation = async (query: string) => {
   try {
-    const response = await fetch(
-      `${API_URL}/geo/1.0/direct?q=${query}&limit=5&appid=${API_KEY}`
+    const res = await axiosInstance.get(
+      `/geo/1.0/direct?q=${query}&limit=5&appid=${API_KEY}`
     );
-    const data = await response.json();
-
-    return data;
+    return res.data;
   } catch (error) {
     console.log(error);
+    throw error;
   }
 };
